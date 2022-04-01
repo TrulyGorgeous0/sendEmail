7b9df55b4aed7c3abdf4ad442e3c99d9ccbd90d7
unknown <trulygorgeous@outlook.com>
2022-04-01 18:20:19 +0100

    first commit

--- models/server.js
+++ models/server.js
@@ -21,7 +21,7 @@ class Server {
   middlewares() {
      
  var corsOptions = {
-  origin: ['http://localhost:3000', 'https://trulygorgeous.co.uk', 'https://zen-albattani-507a08.netlify.app'],
+  origin: ['http://localhost:3000', 'https://trulygorgeous.co.uk', 'https://zen-albattani-507a08.netlify.app', 'https://cristianherreradev.co.uk'],
    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
